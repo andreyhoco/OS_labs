bc770a32acb369508fac8876656ba3d63708db31
andreyhoco <andreygud2013@gmail.com>
2022-03-12 01:21:47 +0300

    Add function print_error

--- lab.c
+++ lab.c
@@ -1,8 +1,18 @@
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+void print_error(int errornum, char* file_name) {
+	char *error_description = strerror(errornum);
+	char error_msg[50];
+	strncpy(error_msg, file_name, 48);
+	strncat(error_msg, ": ", 48 - strlen(error_msg));
+	strncat(error_msg, error_description, 48 - strlen(error_msg));
+	strncat(error_msg, "\n\0", 50 - strlen(error_msg));
+	write(2, error_msg, strlen(error_msg) + 1);
+}
 
 int main() {
-	char* msg = "Hello world!\n\0";
-	write(1, msg, 14);
 	exit(0);
 }
