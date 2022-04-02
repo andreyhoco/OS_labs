4cbdc97fe8719188012dd971a0399af9b07ede73
andreyhoco <andreygud2013@gmail.com>
2022-04-03 00:06:29 +0300

    Add synchronous process launch

--- lab.c
+++ lab.c
@@ -1,25 +1,33 @@
 #include <unistd.h>
 #include <sys/types.h>
+#include <linux/limits.h>
 #include <sys/wait.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_INPUT 2048
+#define INPUT_MAX 2048
+
+int is_stream_empty(FILE* stream);
 
 int main() {
 	pid_t child_pid;
-	char input[MAX_INPUT];
-	char** arguments = calloc(MAX_INPUT + 1, sizeof(char*));
+	char input[INPUT_MAX];
+	char dir[PATH_MAX];
+	char** arguments = calloc(INPUT_MAX + 1, sizeof(char*));
 	int prev_count = 0;
 
 	if (arguments == NULL) {
 		perror(NULL);
 		exit(-1);
 	}
+	getcwd(dir, PATH_MAX);
+
+	printf("Welcome to custom terminal\n");
+	printf("Current dir: %s\n", dir);
 
 	while (1) {
-		if ((fgets(input, MAX_INPUT, stdin) == NULL) && (ferror(stdin) != 0)) {
+		if ((fgets(input, INPUT_MAX, stdin) == NULL) && (ferror(stdin) != 0)) {
 			perror(NULL);
 			continue;
 		}
@@ -30,7 +38,7 @@ int main() {
 
 		int count = 1;
 		char* token = strtok(input, " ");
-		
+
 		if (strchr(token, '\n') != NULL) {
 			if ((arguments[0] = realloc(arguments[0], strlen(token) * sizeof(char))) == NULL) {
 				perror(NULL);
@@ -82,12 +90,36 @@ int main() {
 		}
 		prev_count = count;
 
-		for (int i = 0; i < count; i ++) {
-			printf("\n%d: %s|", i, arguments[i]);
+		child_pid = fork();
+		switch (child_pid) {
+			case -1: {
+				perror(NULL);
+				break;
+			}
+			case 0: {
+				execvp(arguments[0], arguments);
+				break;
+			}
+			default: {
+				if ((count > 1) && (strcmp(arguments[count - 1], "&") == 0)) break;
+				waitpid(child_pid, NULL, 0);
+				break;
+			}
 		}
 	}
 
 	exit(0);
 }
 
-
+int is_stream_empty(FILE* stream) {
+	printf("Check stream");
+	char test;
+	if ((test = getchar()) == EOF) {
+		printf("Empty stream|");
+		printf("Test = %c|", test);	
+		return 1;
+	}
+	else ungetc(test, stream);
+	printf("Test = %c|", test);
+	return 0;
+}
