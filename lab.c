31f178170f066f02f0ea95fd692ba779f469608e
andreyhoco <andreygud2013@gmail.com>
2022-04-23 01:37:43 +0300

    Add writing key in gamma file

--- lab.c
+++ lab.c
@@ -14,8 +14,10 @@ int main(int argc, char* argv[]) {
 	char input_data[INPUT_MAX + 1];
 	char key_data[INPUT_MAX + 1];
 	char output_file[INPUT_MAX];
+	char gamma_file[INPUT_MAX];
 
 	strncpy(output_file, "encrypted_data", sizeof("encrypted_data"));
+	strncpy(gamma_file, "gamma", sizeof("gamma"));
 	memset(input_data, '\0', INPUT_MAX);
 	memset(key_data, '\0', INPUT_MAX);
 
@@ -54,6 +56,16 @@ int main(int argc, char* argv[]) {
 				strncpy(output_file, optarg, INPUT_MAX);
 				break;
 			}
+
+			// case 'g': {
+			// 	if ((strcmp(optarg, "-d") == 0) || (strcmp(optarg, "-k") == 0)){
+			// 		write(2, "-o: arg expected\n\0", strlen("-o: arg expected\n\0") + 1);
+			// 		exit(-1);
+			// 	}
+
+			// 	strncpy(output_file, optarg, INPUT_MAX);
+			// 	break;
+			// }
 		}
 	}
 
@@ -167,19 +179,52 @@ int main(int argc, char* argv[]) {
 		print_error(errno, key_data);
 	}
 
+	int gamma;
+	if ((gamma = open(gamma_file, O_RDWR | O_CREAT, 0664)) == -1) {
+		print_error(errno, gamma_file);
+		exit(-1);
+	}
+
+	char buff[1024];
+	int num_of_readed;
+	while ((num_of_readed = read(key_pipes[0], buff, sizeof(buff))) > 0) {
+		if (write(gamma, buff, num_of_readed) == -1) {
+			print_error(errno, gamma_file);
+			close(gamma);
+			close(input_pipes[0]);
+			close(key_pipes[0]);
+			exit(-1);
+		}
+	}
+	if (num_of_readed == -1) {
+		print_error(errno, "key pipe");
+		close(gamma);
+		close(input_pipes[0]);
+		close(key_pipes[0]);
+		exit(-1);
+	}
+	close(key_pipes[0]);
+
+	if (lseek(gamma, 0, SEEK_SET) == -1) {
+		print_error(errno, gamma_file);
+		close(gamma);
+		close(input_pipes[0]);
+		exit(-1);
+	}
+
 	int output;
 	if ((output = open(output_file, O_WRONLY | O_CREAT, 0664)) == -1) {
 		print_error(errno, output_file);
 		exit(-1);
 	}
 
-	int res = encrypt_data(input_pipes[0], key_pipes[0], output);
+	int res = encrypt_data(input_pipes[0], gamma, output);
 	switch (res) {
 		case -1: {
 			print_error(errno, "input pipe");
 			close(output);
 			close(input_pipes[0]);
-			close(key_pipes[0]);
+			close(gamma);
 			exit(-1);
 		}
 
@@ -187,7 +232,7 @@ int main(int argc, char* argv[]) {
 			print_error(errno, "key pipe");
 			close(output);
 			close(input_pipes[0]);
-			close(key_pipes[0]);
+			close(gamma);
 			exit(-1);
 		}
 
@@ -195,15 +240,14 @@ int main(int argc, char* argv[]) {
 			print_error(errno, output_file);
 			close(output);
 			close(input_pipes[0]);
-			close(key_pipes[0]);
+			close(gamma);
 			exit(-1);
 		}
 	}
 
 	close(output);
+	close(gamma);
 	close(input_pipes[0]);
-	close(key_pipes[0]);
-
 
 	exit(0);
 }
