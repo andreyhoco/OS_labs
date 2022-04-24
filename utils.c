e95ee28d063485d9b2ca33d972fc0477b27dfe3a
andreyhoco <andreygud2013@gmail.com>
2022-04-24 23:39:40 +0300

    Improve encrypt_data function

--- utils.c
+++ utils.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 #define INPUT_MAX 2048
+#define BUFF_SIZE 1024
 
 int parse(char **arguments, char *input_command) {
 	int input_len = strchr(input_command, '\0') == NULL ? strlen(input_command) : strlen(input_command) + 1;
@@ -71,26 +72,28 @@ void free_args(char **args, int args_count) {
 }
 
 int encrypt_data(int data_d, int key_d, int out_d) {
-	char data_buff[1024];
-	char key_buff[1024];
-	char out_buff[1024];
+	char data_buff[BUFF_SIZE];
+	char key_buff[BUFF_SIZE];
+	char out_buff[BUFF_SIZE];
 	int data_readed;
 
 	while ((data_readed = read(data_d, data_buff, sizeof(data_buff))) > 0) {
 		int key_readed;
-		int delta;
 
 		key_readed = read(key_d, key_buff, sizeof(key_buff));
 		if (key_readed == -1) return -2;
 
-		if ((delta = data_readed - key_readed) > 0) {
-			char tmp_buff[1024];
+		while (key_readed < data_readed) {
+			char tmp_buff[BUFF_SIZE];
+			int readed_in_temp;
+			int delta = data_readed - key_readed;
 			if (lseek(key_d, 0, SEEK_SET) == -1) return -2;
 
-			key_readed = read(key_d, tmp_buff, delta);
-			if (key_readed == -1) return -2;
+			readed_in_temp = read(key_d, tmp_buff, delta);
+			if (readed_in_temp == -1) return -2;
 
-			for (int i = key_readed, j = 0; i < (key_readed + delta); i ++, j ++) key_buff[i] = tmp_buff[j];
+			for (int i = key_readed, j = 0; j < readed_in_temp; i ++, j ++) key_buff[i] = tmp_buff[j];
+			key_readed += readed_in_temp;
 		}
 
 		for (int i = 0; i < data_readed; i ++) out_buff[i] = data_buff[i] ^ key_buff[i];
