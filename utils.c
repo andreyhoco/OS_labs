993683d7c57a2bd843124bc30515d99e0e400272
andreyhoco <andreygud2013@gmail.com>
2022-05-06 12:59:40 +0300

    Add double_to_str function

--- utils.c
+++ utils.c
@@ -7,3 +7,31 @@ int free_matrix(unsigned char** matrix, int len) {
 	free(matrix);
 	return 0;
 }
+
+int double_to_str(char* dest, double number, int accuracy) {
+	double num = number;
+	for (int i = 0; i < accuracy; i ++) num *= 10;
+	int int_num = (int) num;
+	char buff[INT_64_LEN + 2];
+	int i = 0;
+
+	while (int_num > 0) {
+		if (i == accuracy) {
+			buff[i] = '.';
+		} else {
+			buff[i] = int_num % 10 + '0';
+			int_num = int_num / 10;
+		}
+		i ++;
+	}
+	if ((number < 1) && (number > -1)) {
+		buff[i] = '.';
+		buff[i + 1] = '0';
+		i += 2;
+	}
+	dest[i] = '\0';
+	i --;
+	for (int j = 0; i >= 0; i --, j ++) dest[j] = buff[i];
+
+	return 0;
+}
--- utils.h
+++ utils.h
@@ -1,3 +1,5 @@
 #define INT_64_LEN 20
 
 int free_matrix(unsigned char** matrix, int len);
+
+int double_to_str(char* dest, double number, int accuracy);
