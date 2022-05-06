35b3bdba8d220361ca8b5ac9d45618bd7e05af9d
andreyhoco <andreygud2013@gmail.com>
2022-05-06 13:00:53 +0300

    Implement concurrent sobel filter image processing

--- lab.c
+++ lab.c
@@ -6,6 +6,8 @@
 #include <sys/stat.h>
 #include <string.h>
 #include <errno.h>
+#include <pthread.h>
+#include <time.h>
 #include "filters.h"
 #include "pnm.h"
 #include "error_handling.h"
@@ -13,12 +15,16 @@
 
 int main() {
 	char image_name[PATH_MAX];
+	char time_name[PATH_MAX];
 	char out_name[PATH_MAX];
+	int threads_num = 4;
 	int image_d;
 
 	memset(image_name, '\0', PATH_MAX);
+	memset(time_name, '\0', PATH_MAX);
 	memset(out_name, '\0', PATH_MAX);
 	strncpy(image_name, "mauer.ppm", strlen("mauer.ppm"));
+	strncpy(time_name, "time", strlen("time"));
 	strncpy(out_name, "sobel.pbm", strlen("sobel.pbm"));
 
 	image_d = open(image_name, O_RDONLY);
@@ -172,7 +178,113 @@ int main() {
 		}
 	}
 
-	apply_sopel_op(grayscale_matrix, sobel_matrix, 1, 1, header.width - 2, header.height - 2);
+	if (threads_num > (header.height - 2)) threads_num = header.height - 2;
+
+	pthread_t** threads = malloc(sizeof(pthread_t*) * threads_num);
+	if (threads == NULL) {
+		free_matrix(sobel_matrix, header.height);
+		free_matrix(grayscale_matrix, header.height);
+		print_error(errno, "memory for threads");
+
+		exit(-1);
+	}
+
+	struct sobel_params** parameters = malloc(sizeof(struct sobel_params*) * threads_num);
+	if (threads == NULL) {
+		free(threads);
+		free_matrix(sobel_matrix, header.height);
+		free_matrix(grayscale_matrix, header.height);
+		print_error(errno, "memory for threads params");
+
+		exit(-1);
+	}
+
+	for (int i = 0; i < threads_num; i ++) {
+		threads[i] = malloc(sizeof(pthread_t));
+		parameters[i] = malloc(sizeof(struct sobel_params));
+
+		if (threads[i] == NULL || parameters[i] == NULL) {
+			free_matrix(sobel_matrix, header.height);
+			free_matrix(grayscale_matrix, header.height);
+
+			for (int k = 0; k < i; k ++) {
+				free(threads[k]);
+				free(parameters[k]);
+			}
+
+			if (threads[i] == NULL) {
+				free(parameters[i]);
+				print_error(errno, "memory for threads");
+			}
+			if (parameters[i] == NULL) {
+				free(threads[i]);
+				print_error(errno, "memory for threads params");
+			}
+			free(threads);
+			free(parameters);
+
+			exit(-1);
+		}
+	}
+
+	for (int i = 0; i < threads_num; i ++) {
+		parameters[i]->source = grayscale_matrix;
+		parameters[i]->dest = sobel_matrix;
+		parameters[i]->from_x = 1;
+		parameters[i]->from_y = header.height / threads_num * i + 1;
+		parameters[i]->to_x = header.width - 2;
+		parameters[i]->to_y = i != (threads_num - 1) ? header.height / threads_num * (i + 1) : header.height - 2;
+	}
+
+	int start = clock();
+
+	for (int i = 0; i < threads_num; i ++) {
+		if (pthread_create(threads[i], NULL, concurency_sobel, parameters[i]) != 0) {
+			free_matrix(sobel_matrix, header.height);
+			free_matrix(grayscale_matrix, header.height);
+
+			for (int k = 0; k < threads_num; k ++) {
+				free(threads[k]);
+				free(parameters[k]);
+			}
+			free(threads);
+			free(parameters);
+
+			print_error(errno, "create thread");
+			exit(-1);
+		}
+	}
+
+	void* res = NULL;
+	for (int i = 0; i < threads_num; i ++) {
+		if (pthread_join(*(threads[i]), res) != 0) {
+			free_matrix(sobel_matrix, header.height);
+			free_matrix(grayscale_matrix, header.height);
+
+			for (int k = 0; k < threads_num; k ++) {
+				free(threads[k]);
+				free(parameters[k]);
+			}
+			free(threads);
+			free(parameters);
+
+			print_error(errno, "join thread");
+			exit(-1);
+		}
+	}
+
+	int end = clock();
+	free_matrix(grayscale_matrix, header.height);
+	for (int k = 0; k < threads_num; k ++) {
+		free(threads[k]);
+		free(parameters[k]);
+	}
+	free(threads);
+	free(parameters);
+
+	double time = ((double)(end - start) / CLOCKS_PER_SEC);
+	char str_time[INT_64_LEN + 2];
+	double_to_str(str_time, time, 5);
 
 	struct pnm_header sobel_header;
 	strncpy(sobel_header.format, "P5", FORMAT_LEN);
@@ -180,7 +292,6 @@ int main() {
 	sobel_header.height = header.height;
 	sobel_header.color_depth = header.color_depth;
 
-	free_matrix(grayscale_matrix, header.height);
 
 	int out = open(out_name, O_WRONLY | O_CREAT, 0664);
 	if (out == -1) {
@@ -207,6 +318,21 @@ int main() {
 			print_format_error("image format");
 			exit(-1);
 		}
+	}
+
+	int time_d = open(time_name, O_WRONLY | O_CREAT | O_APPEND, 0664);
+	if (time_d == -1) {
+		print_error(errno, time_name);
+		write(1, "processing time: ", strlen("processing time: "));
+		write(1, str_time, strlen(str_time));
+	} else {
+		if (write(time_d, str_time, strlen(str_time)) == -1) {
+			print_error(errno, time_name);
+			write(1, "processing time: ", strlen("processing time: "));
+			write(1, str_time, strlen(str_time));
+		}
+		write(time_d, "\n", strlen("\n"));
+		close(time_d);
 	}
 
 	exit(0);
